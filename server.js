afe78c01ebebc8b804bce599f621041f31d24980
1sd <dai1991diver@gmail.com>
2021-04-29 22:23:22 +0900

    [update]Dockerfile追加諸々

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+app.use(express.static('./'))
+
+app.get('/', (req, res) => {
+    res.sendfile('./index.html')
+});
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST);
+
+console.log(`Running on http://${HOST}:${PORT}`);
